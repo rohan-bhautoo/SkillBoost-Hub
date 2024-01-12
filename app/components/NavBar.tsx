7665dc7ea63e1e3b68a624d700bde328e9c08a1d
Rohan Bhautoo <rohanbhautoo@gmail.com>
2024-01-12 21:33:00 +0400

    Adding Buttons in IconBtn

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,31 +1,54 @@
 "use client";
-import { Button, ButtonGroup, HStack, Image, Show } from "@chakra-ui/react";
+import {
+  Button,
+  ButtonGroup,
+  HStack,
+  Hide,
+  IconButton,
+  Image,
+  Show,
+} from "@chakra-ui/react";
 import logo from "../assets/logo.png";
 import CoursesDropdown from "./CoursesDropdown";
 import CustomLink from "./CustomLink";
 import SearchInput from "./SearchInput";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 const NavBar = () => {
   return (
     <HStack padding="8px" gap={4}>
       <CustomLink href="/" label="">
-        <Image src={logo.src} boxSize="120px" h="auto" objectFit="cover" />
+        <Image
+          src={logo.src}
+          maxW={{ base: "70px", md: "90px" }}
+          h="auto"
+          objectFit="cover"
+        />
       </CustomLink>
-      <Show above="lg">
+      <Show above="md">
         <ButtonGroup>
           <CoursesDropdown />
         </ButtonGroup>
       </Show>
       <SearchInput />
-      <ButtonGroup spacing="4">
-        <Button bg="none" borderRadius={15} variant="outline">
-          Teach on SkillBoost
-        </Button>
-        <Button colorScheme="blue" borderRadius={15}>
-          Log in
-        </Button>
-        <Button borderRadius={15}>Sign up</Button>
-      </ButtonGroup>
+      <Show above="md">
+        <ButtonGroup spacing="4">
+          <Button bg="none" borderRadius={15} variant="outline">
+            Teach on SkillBoost
+          </Button>
+          <Button colorScheme="blue" borderRadius={15}>
+            Log in
+          </Button>
+          <Button borderRadius={15}>Sign up</Button>
+        </ButtonGroup>
+      </Show>
+      <Hide above="md">
+        <IconButton
+          aria-label="Open Menu"
+          icon={<HamburgerIcon />}
+          borderRadius={15}
+        />
+      </Hide>
     </HStack>
   );
 };
