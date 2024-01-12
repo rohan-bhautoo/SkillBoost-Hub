76a6f87f70d11e1fb3704955bae5aeef9bbf4686
Rohan Bhautoo <rohanbhautoo@gmail.com>
2024-01-12 17:18:30 +0400

    Updating StaleTime

--- app/components/CoursesDropdown.tsx
+++ app/components/CoursesDropdown.tsx
@@ -55,7 +55,7 @@ const useCategories = () =>
     queryKey: ["categories"],
     queryFn: () =>
       axios.get<Category[]>("/api/categories").then((res) => res.data),
-    staleTime: 60 * 1000, //60s
+    staleTime: 60 * 1440 * 1000, //24h
     retry: 3,
   });
 
