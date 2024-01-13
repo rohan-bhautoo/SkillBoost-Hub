e25bbe0e2df2d49c4ece8c8e8e19b21c99f6862f
Rohan Bhautoo <rohanbhautoo@gmail.com>
2024-01-13 10:47:22 +0400

    Building the Resgistration API

---
+++ app/api/auth/authOptions.ts
@@ -0,0 +1,43 @@
+import prisma from "@/prisma/client";
+import bcrypt from "bcrypt";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Sign in",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) return null;
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user) return null;
+
+        const passowrdsMatch = await bcrypt.compare(
+          credentials.password,
+          user.password!
+        );
+
+        return passowrdsMatch ? user : null;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+};
+
+export default authOptions;
--- app/auth/authOptions.ts
+++
@@ -1,8 +0,0 @@
-import prisma from "@/prisma/client";
-import { NextAuthOptions } from "next-auth";
-
-const authOptions: NextAuthOptions = {
-  providers: [],
-};
-
-export default authOptions;
