7b568b6b0e1d1124943c6cabc1e4a9d277eecc4c
Rohan Bhautoo <rohanbhautoo@gmail.com>
2024-01-21 16:55:44 +0400

    Updating Course Card

--- app/components/CourseCard.tsx
+++ app/components/CourseCard.tsx
@@ -4,6 +4,7 @@ import { Box, Card, CardBody, Image, Text } from "@chakra-ui/react";
 import { Course, Instructor } from "@prisma/client";
 import noImage from "../assets/no-image-placeholder.webp";
 import CourseLevelBadge from "./CourseLevelBadge";
+import Link from "next/link";
 
 interface Props {
   course: Course;
@@ -12,54 +13,56 @@ interface Props {
 
 const CourseCard = ({ course, instructor }: Props) => {
   return (
-    <Card>
-      <CardBody p={0}>
-        <Image
-          src={course.thumbnail ?? noImage.src}
-          w="100%"
-          h="250px"
-          objectFit="cover"
-        />
+    <Link href={`/courses/${course.id}`}>
+      <Card>
+        <CardBody p={0}>
+          <Image
+            src={course.thumbnail ?? noImage.src}
+            w="100%"
+            h="250px"
+            objectFit="cover"
+          />
 
-        <Box p={6}>
-          <Box display="flex" alignItems="baseline">
-            <CourseLevelBadge level={course.level} />
-          </Box>
-          <Box
-            mt="1"
-            fontSize="lg"
-            fontWeight="semibold"
-            as="h2"
-            lineHeight="tight"
-            noOfLines={1}
-          >
-            {course.title}
-          </Box>
+          <Box p={6}>
+            <Box display="flex" alignItems="baseline">
+              <CourseLevelBadge level={course.level} />
+            </Box>
+            <Box
+              mt="1"
+              fontSize="lg"
+              fontWeight="semibold"
+              as="h2"
+              lineHeight="tight"
+              noOfLines={1}
+            >
+              {course.title}
+            </Box>
 
-          <Text fontSize="xs">
-            By {instructor.firstName} {instructor.lastName}
-          </Text>
+            <Text fontSize="xs">
+              By {instructor.firstName} {instructor.lastName}
+            </Text>
 
-          <Box display="flex" mt="2" alignItems="center">
-            {Array(5)
-              .fill("")
-              .map((_, i) => (
-                <StarIcon
-                  key={i}
-                  color={i < course.reviewRating! ? "yellow.500" : "gray.300"}
-                />
-              ))}
-            <Box as="span" ml="2" color="gray.600" fontSize="sm">
-              20 reviews
+            <Box display="flex" mt="2" alignItems="center">
+              {Array(5)
+                .fill("")
+                .map((_, i) => (
+                  <StarIcon
+                    key={i}
+                    color={i < course.reviewRating! ? "yellow.500" : "gray.300"}
+                  />
+                ))}
+              <Box as="span" ml="2" color="gray.600" fontSize="sm">
+                20 reviews
+              </Box>
+            </Box>
+
+            <Box mt={2} fontWeight="bold" fontSize="xl">
+              ${course.price}
             </Box>
           </Box>
-
-          <Box mt={2} fontWeight="bold" fontSize="xl">
-            ${course.price}
-          </Box>
-        </Box>
-      </CardBody>
-    </Card>
+        </CardBody>
+      </Card>
+    </Link>
   );
 };
 
--- app/courses/[id]/page.tsx
+++ app/courses/[id]/page.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { cache } from "react";
+import prisma from "@/prisma/client";
 
-const CourseDetailPage = () => {
-  return <div>CourseDetailPage</div>;
+interface Props {
+  params: { id: string };
+}
+
+const fetchCourse = cache((courseId: number) =>
+  prisma.course.findUnique({
+    where: {
+      id: courseId,
+    },
+  })
+);
+
+const CourseDetailPage = async ({ params }: Props) => {
+  const course = await fetchCourse(parseInt(params.id));
+
+  return <div>CourseDetailPage {course?.id}</div>;
 };
 
 export default CourseDetailPage;
