13e798b513862afe075338b9409f9045d84bd6f6
Rohan Bhautoo <rohanbhautoo@gmail.com>
2024-02-16 20:21:38 +0400

    Creating the CartPage Template

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { Box, GridItem, Heading, SimpleGrid, Text } from "@chakra-ui/react";
 
 const CartPage = () => {
-  return <div>CartPage</div>;
+  return (
+    <SimpleGrid
+      columns={{ base: 1, sm: 1, md: 2, lg: 3, xl: 3 }}
+      spacing={{ base: 0, sm: 0, md: 5, lg: 5, xl: 5 }}
+      px={{ md: 5, lg: 10 }}
+    >
+      <GridItem colSpan={{ base: 1, sm: 1, md: 1, lg: 2, xl: 2 }}>
+        <Box>
+          <Heading>Cart</Heading>
+        </Box>
+      </GridItem>
+      <GridItem>
+        <Text>Payment Info</Text>
+      </GridItem>
+    </SimpleGrid>
+  );
 };
 
 export default CartPage;
